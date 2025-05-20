1d33ee695e53d54deadabffbf69c7c1d06cf3b6b
akins_dev <bobemmanuel503@gmail.com>
2025-05-20 02:20:01 +0100

    initial commit

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { SimpleStorage } from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        console.log("SimpleStorage deployed to:", address(simpleStorage));
+        return simpleStorage;
+    }
+}
+
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3 ==> localhost
+// 0x469EB9FB31c177d4BB3fEb1a9F882dF47F39bD04 ==> Sepolia
---
+++ src/SimpleStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SimpleStorage {
+    uint256 public myFavoriteNumber;
+
+    // uint256[] listOfFavoriteNumbers;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public dev = Person(1, "Emmanuel");
+ 
+    Person[] public allDevs;
+
+    mapping(string => uint256)  public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function addDev(uint256 _favoriteNumber, string memory _name) public {
+        allDevs.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function getFavoriteNumber() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+
+}
